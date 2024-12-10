6aef6f3a8d9242a3762eb7e65eb5c090be4ca56d
jiva_z <jiva_z@naver.com>
2024-12-10 15:09:47 +0900

    [level 1] Title: 음양 더하기, Time: 0.30 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i] == True:
+            answer += int(absolutes[i])
+        else:
+            answer -= int(absolutes[i]) 
+    
+    return answer
