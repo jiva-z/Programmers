e599d876062b0f6aa93dabc98857dd6682af05a1
jiva_z <jiva_z@naver.com>
2025-03-18 21:52:06 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        answer = n;
+        for(int i = 1; i <= t; i++){
+            answer *= 2;
+        }
+        
+        
+        return answer;
+    }
+}
