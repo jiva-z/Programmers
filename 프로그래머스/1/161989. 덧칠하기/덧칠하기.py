4cda07abc049f1f50596e625797f73fbd41c2e64
jiva_z <jiva_z@naver.com>
2025-04-03 19:04:35 +0900

    [level 1] Title: 덧칠하기, Time: 4.47 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,13 +1,13 @@
 def solution(n, m, section):
     answer = 0
-    last_painted = 0
+    last_painted = 0  # 마지막으로 칠한 구역
     
     for s in section:
-        if s > last_painted:
-            answer += 1
-            last_painted = s + m - 1
+        if s > last_painted:  # 아직 칠하지 않은 구역이면
+            answer += 1  # 롤러 사용 횟수 증가
+            last_painted = s + m - 1  # 현재 롤러로 칠할 수 있는 구역의 끝
         
-         # 만약 마지막 구역을 넘어간다면, n으로 제한
+        # 만약 마지막 구역을 넘어간다면, n으로 제한
         if last_painted > n:
             last_painted = n
         
